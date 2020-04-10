a44f8a06548f6289d429e9da9754f1b9f5c10714
Dmitry Birin <dmitry.birin@pipedrive.com>
2020-04-11 02:13:19 +0300

    comments added to game context

--- src/GameContext.js
+++ src/GameContext.js
@@ -13,7 +13,14 @@ const GameContext = createContext()
 
 export const GameContextProvider = ({ children }) => {
 	const [state, setState] = useState(initialState)
-	const setGameState = obj => setState(state => ({ ...state, ...obj }))
+	const setGameState = obj => {
+		// TODO make tests then warnings and logging state.hasOwnProperty
+		// TODO make logging only for dev
+		// if(process.env.NODE_ENV === 'DEV') {
+		// 	console.log('Implementing upon state', obj)
+		// }
+		setState(state => ({ ...state, ...obj }))
+	}
 
 	return <GameContext.Provider value={[state, setGameState]}>{children}</GameContext.Provider>
 }
